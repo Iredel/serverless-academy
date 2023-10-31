d9c33458a36c3427f783d87873bdb482f8093cfd
Nikita Komarov <nikitakomarov2143@gmail.com>
2023-10-31 12:45:55 +0200

    added seventh solved task

---
+++ 07_grouping_vacations/main.js
@@ -0,0 +1,34 @@
+const vacationsData = require('./data.json')
+
+const groupVacations = (data) =>{
+    const gruopedData = data.reduce((acc, obj) =>{
+        const existingUser = acc.find((user) => user.userId === obj.user._id)
+        if(existingUser){
+            existingUser.vacations.push({
+                usedDays: obj.usedDays,
+                startDate: obj.startDate,
+                endDate: obj.endDate,
+                "status": obj.status
+            })
+        }else{
+            const user = obj = {
+                userId: obj.user._id,
+                userName: obj.user.name,
+                vacations:[
+                    {
+                        "usedDays": obj.usedDays,
+                        "startDate": obj.startDate,
+                        "endDate": obj.endDate,
+                        "status": obj.status
+                    }
+                ]
+            }
+            acc.push(user)
+        }
+        return acc
+    }, [])
+
+    return gruopedData
+}
+
+console.log(groupVacations(vacationsData))
